0cd951fed93f17ee3f4ed93f0a2f30c65bdf4ce9
Stephen Fowler <stephen.nicholas.fowler@gmail.com>
2024-08-15 15:47:44 +1000

    get the square of a number

---
+++ square.js
@@ -0,0 +1,9 @@
+function calculateSquare() {
+    const quantity = document.getElementById('quantity').value;
+    if (quantity >= 1 && quantity <= 4) {
+        const square = quantity * quantity;
+        document.getElementById('result').textContent = `The square of ${quantity} is ${square}.`;
+    } else {
+        document.getElementById('result').textContent = 'Please enter a number between 1 and 4.';
+    }
+}
